1e3b4db7923c52e76f35073cad103fa2b4ae3b63
David Laubenstein <david.laubenstein@posteo.de>
2017-01-08 20:03:56 +0100

    correct indexes

--- weather.py
+++ weather.py
@@ -2,13 +2,12 @@
 # -*- coding: utf-8 -*-
 
 # Prints the following basic weather data:
-# - City name
 # - Weather conditions (e.g. rain, cloudy, etc.)
 # - Temperature (current, hi, lo)
 #
 # Weather data source: OpenWeatherMap http://openweathermap.org (API v2.5)
 #
-# Copyright 2013-2014 Dennis Ideler (ideler.dennis@gmail.com)
+# Copyright 2017 by David Laubenstein 
 
 from colorama import init, Fore, Back, Style
 import requests
@@ -18,18 +17,29 @@ import sys
 CITY_ID = str('2892794') # St. Catharines, Ontario, Canada.
 UNITS = 'metric' # Celsius (use 'imperial' for Fahrenheit).
 APP_ID = str('7aa91ff4cf71825f909554def466be78')
-COUNT = sys.argv[1]
+#handle arguments. and set parameter COUND and COUNT_Info
+if (len(sys.argv) == 2):  
+    COUNT = str(int(sys.argv[1]) + 1)
 
-try: 
-    if (int(sys.argv[1]) <= 16):
-        COUNT_INFO = int(sys.argv[1]) - 1
-    else:
+    try: 
+        if (int(sys.argv[1]) <= 16):
+            if(int(sys.argv[1] != 0)):
+                COUNT_INFO = int(sys.argv[1])
+            else:
+                COUNT_INFO = 0
+        else:
+            sys.exit(1)
+
+    except:
+        print("The argument you entered is not valid! \n"
+	      "You have to enter an Integer between 0 and 16, which is the \n"
+	      "days after today... ")
         sys.exit(1)
 
-except:
-    print("The argument you entered is not valid! \n"
-	  "You have to enter an Integer between 0 and 16, which is the \n"
-	  "days after today... ")
+elif (len(sys.argv) == 1):
+    COUNT = "0";
+    COUNT_INFO = 0;
+else:
     sys.exit(1)
 
 QUERY = '?id={}&units={}'.format(CITY_ID, UNITS)
